aeb6dbf489d4e8b77593396781f8be5815c4bc0a
tugce <tgcedal@gmail.com>
2018-02-18 19:51:20 +0300

    Application classe's annotations added

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -1,9 +1,16 @@
 package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages={"com.example.todo"})
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class TodoApplication {
 
 	public static void main(String[] args) {
